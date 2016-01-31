efb02ddd3a47450811639b7e6acf0ee544fb24aa
Alvin Wan <hi@alvinwan.com>
2016-01-31 13:02:02 -0800

    changing to find_packages

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
 	name='Textmining',
 	version='0.0.1',
 	description='statistical text mining',
-	packages=['textmining', 'textmining.*', 'textmining.data.*', 'textmining.examples.*'],
+	packages=find_packages(),
 	package_data={'': ['*.txt']}
 )
