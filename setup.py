17a010e15d8900af36e5cfd970c20ef10bac7414
Alvin Wan <hi@alvinwan.com>
2016-01-31 12:56:30 -0800

    adding all files

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
 	name='Textmining',
 	version='0.0.1',
 	description='statistical text mining',
-	packages=find_packages(),
+	packages=['textmining', 'textmining/data/*', 'textmining/examples/*'],
 	package_data={'': ['*.txt']}
 )
